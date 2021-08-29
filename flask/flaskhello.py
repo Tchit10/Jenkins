0955176f626e3ea69f3da6e593d8f2649bd16ffd
Tchit10 <40052574+Tchit10@users.noreply.github.com>
2021-08-29 17:09:54 +0200

    Update flaskhello.py

--- flask/flaskhello.py
+++ flask/flaskhello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world with Github Push Now"
+    return "Hello world with Jenkins and my Github Repo !!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
