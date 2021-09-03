c9d56457a0c5a5044930f430cb73ec83d13db4c0
Tchit10 <40052574+Tchit10@users.noreply.github.com>
2021-09-03 13:47:41 +0200

    Update flaskhello.py

--- flask/flaskhello.py
+++ flask/flaskhello.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-"""
-Little Web App for tests.
-"""
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Publish little Web App for tests."""
     return "Hello world with Jenkins and my Github Repo !!!"
 
 if __name__ == "__main__":
